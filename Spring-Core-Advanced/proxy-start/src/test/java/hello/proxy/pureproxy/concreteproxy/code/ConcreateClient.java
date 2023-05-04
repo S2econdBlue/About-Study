1fa5bc899184c650a5757de87601d806d390a5c4
이청 <s2econd.blue@gmail.com>
2023-05-04 14:56:35 +0900

    중간 업로드

---
+++ Spring-Core-Advanced/proxy-start/src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreateClient.java
@@ -0,0 +1,12 @@
+package hello.proxy.pureproxy.concreteproxy.code;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ConcreateClient {
+    private final ConcreateLogic concreateLogic;
+
+    public void execute(){
+        concreateLogic.operation();
+    }
+}
---
+++ Spring-Core-Advanced/proxy-start/src/test/java/hello/proxy/pureproxy/concreteproxy/code/TimeProxy.java
@@ -0,0 +1,22 @@
+package hello.proxy.pureproxy.concreteproxy.code;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class TimeProxy extends ConcreateLogic{
+    private final ConcreateLogic concreateLogic;
+
+    @Override
+    public String operation() {
+        log.info("TimeDecorator 실행");
+        long startTime = System.currentTimeMillis();
+        String result = concreateLogic.operation();
+        long endTime = System.currentTimeMillis();
+
+        long resultTime = endTime - startTime;
+        log.info("TimeDecorator 종료 resultTime={}ms", resultTime);
+        return result;
+    }
+}

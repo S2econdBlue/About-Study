5c19c79ceff9f22d2068fa5f6970b0eeec7db6ae
이청 <s2econd.blue@gmail.com>
2023-05-27 23:53:55 +0900

    불필요한 javaFX 프로젝트 삭제

--- FXstart/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.fxstart {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires kotlin.stdlib;
-
-
-    opens com.example.fxstart to javafx.fxml;
-    exports com.example.fxstart;
-}
--- JavaFXWithJava/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.javafxwithjava {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.javafxwithjava to javafx.fxml;
-    exports com.example.javafxwithjava;
-}
